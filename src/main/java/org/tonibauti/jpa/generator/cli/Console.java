9ab0c03234b814ba71413077af45ff67235f350e
tonibauti <ableon.fs@gmail.com>
2022-09-09 09:24:55 +0200

    add getList function to repositories

--- src/main/java/org/tonibauti/jpa/generator/cli/Console.java
+++ src/main/java/org/tonibauti/jpa/generator/cli/Console.java
@@ -14,7 +14,7 @@ public class Console
     public static final String line = "----------------------------------------" + lineSeparator;
 
     protected static final String BANNER   = "JPA - Generator";
-    protected static final String VERSION  = "1.0.1";
+    protected static final String VERSION  = "1.1.0";
     protected static final String CLI_NAME = "jpa-generator";
     protected static final String URL      = "https://github.com/tonibauti";
 
